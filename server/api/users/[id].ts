8b0656a1b04f8d9915aca46e3945a4a4ad6a4e45
Albert <a@asyo.dev>
2024-02-25 13:55:38 +0000

    user test API

---
+++ server/api/users/[id].ts
@@ -0,0 +1,31 @@
+export default defineEventHandler((event) => {
+  const name = getRouterParam(event, "id");
+
+  return {
+    name: name,
+    medicalConditions: [
+      "A",
+      "B",
+      "C"
+    ],
+    bloodType: "O+",
+    allergies: [
+      "D",
+      "E",
+      "F"
+    ],
+    currentMedicines: [
+      "DRUG",
+      "MORE DRUG"
+    ],
+    weight: 12.3,
+    height: 1.85,
+    dateOfBirth: Date.now(),
+    address: "Woodland road",
+    organDoner: false,
+    medicalNotes: [
+      "note 1",
+      "note 2"
+    ]
+  }
+})
