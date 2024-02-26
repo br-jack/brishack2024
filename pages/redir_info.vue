9871a61304c0730cc23838ca90cd8aac7d1a08fc
Albert <a@asyo.dev>
2024-02-26 08:25:56 +0000

    Better table (from aidan in [id])

--- pages/redir_info.vue
+++ pages/redir_info.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
-import { definePageMeta } from '#imports'
-definePageMeta({
-  auth: {
-    unauthenticatedOnly: false,
-    navigateAuthenticatedTo: '/',
-  }
-})
-const { signIn, token, data, status, lastRefreshedAt } = useAuth()
+const { token, data } = useAuth()
 if (!token.value) {
   await navigateTo("/login")
 }
-const info = await $fetch(`/api/users/${data.value?.username}`, {
-  // @ts-ignore
+const information = await $fetch(`/api/users/${data.value?.username}`, {
   headers: {
     "Authorization": token
   }
@@ -30,24 +22,68 @@ interface medication {
 
 <template>
   <div>
-    <h1>Edit user information</h1>
+    <h1>User information</h1>
 
-    <NuxtLink to="/add_meds">Add Medication</NuxtLink>
-    <NuxtLink to="/add_info">Add Information</NuxtLink>
+    <ClientOnly>
+      <div class="row justify-evenly content-between q-gutter-sm">
+        <NuxtLink to="/add_meds">
+          <q-btn color="blue" text-color="white" label="Add Medication" />
+        </NuxtLink>
+        <NuxtLink to="/add_info">
+          <q-btn color="blue" text-color="white" label="Add Information" />
+        </NuxtLink>
+        <v-table :data="information">
+          <thead slot="head">
+            <th>Name</th>
+            <th>Phone Number</th>
+            <th>Date of Birth</th>
+            <th>Blood Type</th>
+            <th>Organ Donor Status</th>
+            <th>Info Available to All Users</th>
+          </thead>
+          <tr>
+            <td>{{ information.Name }}</td>
+            <td>{{ information.Number }}</td>
+            <td>{{ new Date(information.DateOfBirth).toDateString() }}</td>
+            <td>{{ information.BloodType }}</td>
+            <td>{{ information.OrganDonor ? "Yes" : "No" }}</td>
+            <td>{{ information.InfoPublicallyAvailable ? "Yes" : "No" }}</td>
+          </tr>
+        </v-table>
+      </div>
+      <br>
+      <div class="row justify-evenly content-between q-gutter-sm">
+        <v-table :data="information.medications">
+          <thead slot="head">
+            <th>Medication</th>
+            <th>Dose Frequency</th>
+            <th>Additional Notes</th>
+          </thead>
+          <tbody slot="body">
+            <tr v-for="row in information.medications">
+              <td>{{ row.MedicationName }}</td>
+              <td>{{ row.DoseFrequency }}</td>
+              <td>{{ row.AdditionalNotes }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </div>
+      <br>
+      <div class="row justify-evenly content-between q-gutter-sm">
+        <v-table :data="information.information">
+          <thead slot="head">
+            <th>Information</th>
+            <th>Additional Information</th>
+          </thead>
+          <tbody slot="body">
+            <tr v-for="row in information.information">
+              <td>{{ row.InfoName }}</td>
+              <td>{{ row.AddInfo }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </div>
 
-    <template v-for="inf in info.information">
-      <input v-model="inf.InfoName" type="text" />
-      <input v-model="inf.AddInfo" type="text" />
-    </template>
-
-    <template v-for="meds in info.medications">
-      <input v-model="meds.MedicationName" type="text" />
-      <input v-model="meds.DoseFrequency" type="text" />
-      <input v-model="meds.AdditionalNotes" type="text" />
-    </template>
-
-    <pre>Data: {{ data || 'no session data present, are you logged in?' }}</pre>
-    <pre>Last refreshed at: {{ lastRefreshedAt || 'no refresh happened' }}</pre>
-    <pre>JWT token: {{ token || 'no token present, are you logged in?' }}</pre>
+    </ClientOnly>
   </div>
 </template>
