ddba3057b9593daecea9235636d6bc0c97e0f775
Albert <a@asyo.dev>
2024-02-26 03:51:10 +0000

    Uses session username

--- server/api/info/med.post.ts
+++ server/api/info/med.post.ts
@@ -3,12 +3,11 @@ import { insertMedication } from "../dbFuncs";
 
 export default eventHandler(async (event) => {
   interface bodyData {
-    civUsername: string;
     medicationName: string;
     doseFrequency: string;
     additionalInfo: string;
   }
   const result: bodyData = await readBody(event);
   const session = ensureAuth(event);
-  insertMedication(result.civUsername, result.medicationName, result.doseFrequency, result.additionalInfo)
+  insertMedication(session.username, result.medicationName, result.doseFrequency, result.additionalInfo)
 });
