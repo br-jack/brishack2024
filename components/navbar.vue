758a32c8a24b03037b2fda80fdad3f74cfaedca3
Albert <a@asyo.dev>
2024-02-26 07:04:57 +0000

    nice navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,6 +1,6 @@
-<script setup>
-const { status, signOut } = useAuth()
-const clearStorage = function (key) {
+<script setup lang=ts>
+const { status, signOut, data } = useAuth()
+const clearStorage = function (key: any) {
   if (key) {
     localStorage.removeItem(key);
   } else {
@@ -15,20 +15,32 @@ const clearStorage = function (key) {
       <NuxtLink to='/login'>
         <q-btn color="white" text-color="black" label="Login" />
       </NuxtLink>
-      <NuxtLink to='/add_info'>
-        <q-btn color="white" text-color="black" label="Login" />
-      </NuxtLink>
     </div>
-    <q-btn v-if="status === 'authenticated'" color="white" @click="clearStorage('token'); signOut({ redirect: false })"
-      text-color="black" label="Signout" />
-    <NuxtLink to='/add_info'>
-      <q-btn color="white" text-color="black" label="Add Info" />
-    </NuxtLink>
+    <div v-if="status === 'authenticated'" class="q-gutter-sm">
+      <NuxtLink to='/add_info'>
+        <q-btn color="white" text-color="black" label="Add Info" />
+      </NuxtLink>
+      <NuxtLink to='/add_meds'>
+        <q-btn color="white" text-color="black" label="Add Meds" />
+      </NuxtLink>
+      <NuxtLink to='/edit_tags'>
+        <q-btn color="white" text-color="black" label="Edit Tags" />
+      </NuxtLink>
+      <NuxtLink v-if="data?.type === 'INT'" to='/med_reg'>
+        <q-btn color="white" text-color="black" label="Register Meds" />
+      </NuxtLink>
+      <q-btn color="white" class="fix" @click="clearStorage('token'); signOut({ redirect: false })" text-color="black"
+        label="Signout" />
+    </div>
   </nav>
 </template>
 
 <style scoped>
 nav {
   background: #3ea;
+}
+
+.fix {
+  margin-top: 0;
 }
 </style>
