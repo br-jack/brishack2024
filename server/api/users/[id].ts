e9356eed64c861437f986f9212eb67320537a7d5
Aidan <aidanmadge@gmail.com>
2024-02-26 00:49:29 +0000

    Revert "move thingsss"
    This reverts commit 071fc0d6491f00b42f9b66bf83b263c7e7834706.

--- server/api/users/[id].ts
+++ server/api/users/[id].ts
@@ -1,6 +1,6 @@
 import { ensureAuth } from '../auth/session.get';
-import { databaseFillData } from '../dbStuff/dbDummyData';
-import { getUserInfo } from '../dbStuff/dbFuncs';
+import { databaseFillData } from '../dbDummyData';
+import { getUserInfo } from '../dbFuncs';
 
 export default defineEventHandler(async (event) => {
   const name = getRouterParam(event, "id");
